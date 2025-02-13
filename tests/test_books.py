d6f95df39c7e2059be06050d997f56957aba4819
AMADIVHACKERDON <101997952+AMADIVHACKERDON@users.noreply.github.com>
2025-02-13 20:13:41 +0100

    Update test_books.py

--- tests/test_books.py
+++ tests/test_books.py
@@ -4,14 +4,14 @@ from api.db.schemas import Genre
 
 client = TestClient(app)
 def test_get_all_books():
-    response = client.get("/api/v1/books/")
+    response = client.get("/books/")
     assert response.status_code == 200
     assert isinstance(response.json(), dict)  # Ensure it returns a dictionary
     assert len(response.json()) > 0  # Ensure there is at least 1 book
 
 
 def test_get_single_book():
-    response = client.get("/api/v1/books/1")
+    response = client.get("/books/1")
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit"
@@ -26,7 +26,7 @@ def test_create_book():
         "publication_year": 1997,
         "genre": Genre.FANTASY,
     }
-    response = client.post("/api/v1/books/", json=new_book)
+    response = client.post("/books/", json=new_book)
     assert response.status_code == 201
     data = response.json()
     assert data["id"] == 4
@@ -41,7 +41,7 @@ def test_update_book():
         "publication_year": 1937,
         "genre": "Fantasy",
     }
-    response = client.put("/api/v1/books/1", json=updated_book)
+    response = client.put("/books/1", json=updated_book)
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit: An Unexpected Journey"
@@ -49,7 +49,7 @@ def test_update_book():
 
 def test_delete_book():
     client.post(
-        "/api/v1/books/",
+        "/books/",
         json={
             "id": 99,
             "title": "Temporary Book",
@@ -60,7 +60,7 @@ def test_delete_book():
     )
 
     # Now delete it
-    response = client.delete("/api/v1/books/99")
+    response = client.delete("/books/99")
     assert response.status_code == 204
 
     # Ensure it's gone
