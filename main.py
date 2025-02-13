275cd656254fea96707271fa220fff1f7e9bdd8d
AMADIVHACKERDON <101997952+AMADIVHACKERDON@users.noreply.github.com>
2025-02-13 17:24:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from books import app as books_app
 from fastapi.middleware.cors import CORSMiddleware
-
+from api.routes import book_routes
 from api.router import api_router
 from core.config import settings
 
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(books_app, prefix="/api")
+app.include_router(book_routes.router, prefix="/books", tags=["books"])
 
 
 @app.get("/healthcheck")
