f419b75d79541d4d18f0001df97a7d6cad1f7835
AMADIVHACKERDON <101997952+AMADIVHACKERDON@users.noreply.github.com>
2025-02-15 19:06:41 +0100

    Update test_books.py

--- tests/test_books.py
+++ tests/test_books.py
@@ -4,7 +4,7 @@ from api.db.schemas import Genre
 
 client = TestClient(app)
 def test_get_all_books():
-    response = client.get("/books/")
+    response = client.get("/api/v1/books/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)  # Ensure it returns a dictionary
     assert len(response.json()) > 0  # Ensure there is at least 1 book
@@ -12,7 +12,7 @@ def test_get_all_books():
 
 
 def test_get_single_book():
-    response = client.get("/books/1")
+    response = client.get("/api/v1/books/1")
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit"
@@ -27,7 +27,7 @@ def test_create_book():
         "publication_year": 1997,
         "genre": Genre.FANTASY,
     }
-    response = client.post("/books/", json=new_book)
+    response = client.post("/api/v1/books/", json=new_book)
     assert response.status_code == 201
     data = response.json()
     assert data["id"] == 4
@@ -42,7 +42,7 @@ def test_update_book():
         "publication_year": 1937,
         "genre": "Fantasy",
     }
-    response = client.put("/books/1", json=updated_book)
+    response = client.put("/api/v1/books/1", json=updated_book)
     assert response.status_code == 200
     data = response.json()
     assert data["title"] == "The Hobbit: An Unexpected Journey"
@@ -50,7 +50,7 @@ def test_update_book():
 
 def test_delete_book():
     client.post(
-        "/books/",
+        "/api/v1/books/",
         json={
             "id": 99,
             "title": "Temporary Book",
@@ -61,9 +61,9 @@ def test_delete_book():
     )
 
     # Now delete it
-    response = client.delete("/books/99")
+    response = client.delete("/api/v1/books/99")
     assert response.status_code == 204
 
     # Ensure it's gone
-    response = client.get("/books/99")
+    response = client.get("/api/vi/books/99")
     assert response.status_code == 404
